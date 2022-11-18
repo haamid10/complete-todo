3a4a3d71a9ab36c41a3aa9afaf4da6fcb00a63cd
haamid10 <apdixamiid919@gmail.com>
2022-11-18 11:35:25 +0300

    ternery

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const x="baasiro"
+  const a = true
   return (
     <div className="App">
       <h1> Hello From React</h1>
-      <h2>hello</h2>
+      <h2>hello {x == 'baasiro' ?'yes' : 'mo'}</h2>
     </div>
   );
 }
