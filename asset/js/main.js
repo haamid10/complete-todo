ea45394e8cf1c2734f7f8459617b35afe9d5c779
haamid10 <apdixamiid919@gmail.com>
2022-11-18 08:46:11 +0300

    main

--- asset/js/main.js
+++
@@ -1,8 +0,0 @@
-const fl=gsap.timeline({defaults:{duration: 0.75,ease:"Power3.easeOut"},
-}
-);
-
-
-
-fl.fromTo(".header",{y:-90 ,opacity:0},{y:0, opacity:1},"<20%")
-fl.fromTo(".logo",{x:-90 ,opacity:0},{x:0, opacity:1},"<")
