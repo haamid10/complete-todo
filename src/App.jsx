91105bf6be703c454a33b6175caf93457fe7648b
haamid10 <apdixamiid919@gmail.com>
2022-11-19 14:46:48 +0300

    still passing the props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
 import Header from "./components/Header"
 import Tasks from './components/Tasks';
 
-import "./index.css";
 
 const App = () => {
+ const [tasks,setTasks] = useState(
+  [
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:28pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at the School',
+      day: 'Feb 6th at 3:28pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food shopping',
+      day: 'Feb 7th at 9:28pm',
+      reminder: false,
+    },
+    
+  ]
+ )
   return (
     <div className='container'>App
 <Header />
-<Tasks/>
+<Tasks tasks={tasks}/>
     </div>
   )
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,26 +1,6 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:28pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting at the School',
-    day: 'Feb 6th at 3:28pm',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Food shopping',
-    day: 'Feb 7th at 9:28pm',
-    reminder: false,
-  },
-  
-]
 
-const Tasks = () => {
+
+const Tasks = ({tasks}) => {
   return (
     <div>
       {tasks.map((task)=><h3>{task.id}</h3>)}
