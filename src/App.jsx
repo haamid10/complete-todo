62e1a40f8f62927dcbcecfa2852b0cf452fae369
haamid10 <apdixamiid919@gmail.com>
2022-11-19 10:33:56 +0300

    css in js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from "./components/Header"
 
+
 const App = () => {
   return (
     <div>App
-<Header title={1}/>
+<Header title={'siuu'}/>
     </div>
   )
 }
